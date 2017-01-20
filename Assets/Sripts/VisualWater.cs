fce29f90f23f769a0b344afbc13faab81b9b0c24
Fonserbc <fonserbc@gmail.com>
2017-01-20 20:11:10 +0100

    Firsts water scripts

---
+++ Assets/Sripts/VisualWater.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisualWater : MonoBehaviour {
+
+	public WaterCPU waterCalculator;
+
+	public int width = 32;
+	public int height = 16;
+
+	public Vector2 cubeSize;
+
+	public float[,] waterHeight;
+
+	// Use this for initialization
+	void Start () {
+
+		waterCalculator.Init (width, height);
+
+		waterHeight = new float[width, height];
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		waterCalculator.Step ();
+
+		waterCalculator.RetrieveIntensities (ref waterHeight);
+	}
+}
---
+++ Assets/Sripts/WaterCPU.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterCPU : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	public void Init (int w, int h) {
+		
+	}
+	
+	// Update is called once per frame
+	public void Step () {
+		
+	}
+
+	public void AddWave (Vector2 position, Vector2 wave) {
+	
+	}
+
+	// Matrix is w x h
+	public void RetrieveIntensities (ref float[,] matrix) {
+	
+	}
+}
