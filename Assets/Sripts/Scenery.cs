b7efe1c62e56df750c79289a067f93d4d640391f
Fonserbc <fonserbc@gmail.com>
2017-01-21 21:23:41 +0100

    Fixed billboards, working on scenery

---
+++ Assets/Sripts/Scenery.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scenery : MonoBehaviour {
+
+	public float worldHeight = 2 / 16f;
+
+	public float backFloorWidth = 2f;
+
+	public GameObject poolWall;
+	public GameObject floor;
+
+	// Use this for initialization
+	void Init (Vector2 poolSize) {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
