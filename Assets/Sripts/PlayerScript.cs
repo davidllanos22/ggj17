24898b6cfb45d104c821404b5105beb3427aba13
Sergi <sergicebriangres@gmail.com>
2017-01-21 16:45:49 +0100

    Step By Step waves on acceleration

--- Assets/Sripts/PlayerScript.cs
+++ Assets/Sripts/PlayerScript.cs
@@ -7,9 +7,13 @@ public class PlayerScript : MonoBehaviour {
     float maxSpeed = 100f;
     float speed = 5000f;
     float waveHeight = 5f;
+    float attackMultiplyer = 5f;
     float potency = 0f;
     float chargeSpeed = 10f;
     float maxPotency = 15f;
+
+    float timer = 0f;
+    float stepWave = .05f;
 
 	public GameController controller;
 
@@ -20,13 +24,21 @@ public class PlayerScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        Vector3 charDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
+        Vector3 charDir = new Vector3(Mathf.Round(Input.GetAxis("Horizontal")), 0, Mathf.Round(Input.GetAxis("Vertical"))).normalized;
+
+        if (timer > 0) timer -= Time.deltaTime;
 
         if (Input.GetAxis("Fire1") > 0.1f || Input.GetKey(KeyCode.K))
         {
             if (rb.velocity.magnitude < maxSpeed) rb.AddForce(Time.deltaTime * charDir * speed);
-            controller.AddWave(transform.position - charDir, -new Vector2(charDir.x, charDir.z) * waveHeight);
+            if (timer <= 0)
+            {
+                controller.AddWave(transform.position - charDir*0.5f, -new Vector2(charDir.x, charDir.z) * waveHeight);
+                timer = stepWave;
+            }
         }
+
+        
 
         if (Input.GetKey(KeyCode.Space))
         {
@@ -35,7 +47,7 @@ public class PlayerScript : MonoBehaviour {
 
         if (Input.GetKeyUp(KeyCode.Space))
         {
-            controller.AddWave(transform.position + charDir, new Vector2(charDir.x, charDir.z) * waveHeight * potency * 5);
+            controller.AddWave(transform.position + charDir, new Vector2(charDir.x, charDir.z) * waveHeight * potency * attackMultiplyer);
             rb.AddForce(Time.deltaTime * -charDir * 2 * speed * potency);
             potency = 0;
         }
